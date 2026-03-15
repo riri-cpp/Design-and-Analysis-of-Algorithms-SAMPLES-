042e564c4fa6449f87ab979b3ce8e8c802efc23a
riri-cpp <pasiarheman7@gmail.com>
2026-03-15 14:16:01 +0800

    deleted comments

--- LongestPalindromicSequence.cpp
+++ LongestPalindromicSequence.cpp
@@ -7,37 +7,28 @@ using namespace std;
 int longestPalindromicSubsequence(string s) {
     int n = s.length();
     
-    // Create a 2D DP table
-    // dp[i][j] represents the length of longest palindromic subsequence in s[i..j]
     vector<vector<int>> dp(n, vector<int>(n, 0));
     
-    // Every single character is a palindrome of length 1
     for (int i = 0; i < n; i++) {
         dp[i][i] = 1;
     }
     
-    // Fill the DP table
-    // We need to consider all substrings of length 2 or more
-    for (int len = 2; len <= n; len++) {  // len is the length of substring
+    for (int len = 2; len <= n; len++) {
         for (int i = 0; i <= n - len; i++) {
-            int j = i + len - 1;  // ending index of substring
+            int j = i + len - 1;
             
             if (s[i] == s[j]) {
-                // If first and last characters match
                 if (len == 2) {
-                    dp[i][j] = 2;  // Two matching characters
+                    dp[i][j] = 2; 
                 } else {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;  // Add 2 and recurse for inner substring
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
                 }
             } else {
-                // If first and last characters don't match
-                // Take maximum of either excluding first or excluding last character
                 dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
             }
         }
     }
     
-    // The answer is dp[0][n-1] (longest palindromic subsequence in entire string)
     return dp[0][n - 1];
 }
 
